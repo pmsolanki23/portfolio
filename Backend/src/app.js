9e23046047c120612c4c9c09b0b60ddc93d1b62c
pmsolanki23 <spruthvi863@gmail.com>
2026-04-22 17:44:02 +0530

    final fix vite base + routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,21 +6,26 @@ import cron from "node-cron";
 import { autoDeleteContacts } from "./controllers/contact.controller.js";
 import projectRoutes from "./routes/project.routes.js";
 import contactRoutes from "./routes/contact.routes.js";
-import authRoutes from "./routes/auth.routes.js"
-import reviewRoutes from "./routes/review.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
 const app = express();
 
 // 🔥 FIX __dirnamea
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
- 
-// 🔥 MIDDLEWARE
-app.use(cors({
-    origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+const __dirname = path.dirname(__filename);
 
-}));
+// 🔥 MIDDLEWARE
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-five-phi-tcfc4kibkx.vercel.app",
+    ],
+
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 // ⏰ Run every day at 12:00 AM
 cron.schedule("0 0 * * *", () => {
@@ -32,7 +37,7 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 // 🔥 ROUTES
 app.use("/api/projects", projectRoutes);
 app.use("/api/contact", contactRoutes);
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api/reviews", reviewRoutes);
 
 app.use((err, req, res, next) => {

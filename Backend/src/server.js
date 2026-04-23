27ae0b1d841f5550ad838afb5c440d9d6852968d
pmsolanki23 <spruthvi863@gmail.com>
2026-04-23 11:21:11 +0530

    final fix vite base + routing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,8 +8,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log("CLOUD NAME 👉", process.env.CLOUDINARY_CLOUD_NAME);
-  console.log("CLOUD 👉", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("KEY 👉", process.env.CLOUDINARY_API_KEY);
-console.log("SECRET 👉", process.env.CLOUDINARY_API_SECRET);
 });
